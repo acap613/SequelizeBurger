adf75879996faf7edf9a9e8b41e93e9bc39ced66
Angelo Capozzi <acap613@gmail.com>
2020-01-11 09:43:14 -0500

    initial commit, copying previous burger app

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('turkyburger');
+INSERT INTO burgers (burger_name) VALUES ('hamburger');
+
+
+
+-- INSERT INTO cats (name) VALUES ('Meeses');
